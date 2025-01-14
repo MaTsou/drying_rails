107f1efe9584af30be071f14fb0f5fa09991ce4d
MaTsou <contact@logicore.fr>
2025-01-14 14:07:41 +0100

    Replace ThyResult usage by MyResult.

--- lib/initializers/container_initializer.rb
+++ lib/initializers/container_initializer.rb
@@ -1,6 +1,13 @@
 require 'dry/system'
 #require 'thy_result'
 
+class MyResult
+  def set( type, value = true, *args )
+    content = args.empty? ? value : args.unshift( value )
+    { type => content }
+  end
+end
+
 class ::DryingContainer < Dry::System::Container
   configure do |config|
     config.root = Rails.root
@@ -11,10 +18,3 @@ class ::DryingContainer < Dry::System::Container
   self.finalize! if Rails.env.production?
 end
 ::Deps = DryingContainer.injector
-
-class MyResult
-  def set( type, value = true, *args )
-    content = args.empty? ? value : args.unshift( value )
-    { type => content }
-  end
-end
