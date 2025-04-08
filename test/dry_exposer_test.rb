ebe40534299d72dc2411b78e98dea22d1001b4e3
MaTsou <contact@logicore.fr>
2025-04-08 19:31:15 +0200

    Make tests rubocop winner.

--- test/dry_exposer_test.rb
+++ test/dry_exposer_test.rb
@@ -1,75 +1,78 @@
-require_relative 'test_helper.rb'
+# frozen_string_literal: true
+
+require_relative 'test_helper'
 require_relative '../lib/initializers/exposer_initializer'
 require 'ustruct'
 
-describe DryExposer do
-  ABSOLUTE = "Hello world"
-  SUFFIX = "I am there"
-  BUDGET = Ustruct.new( id: 123456 )
+SUFFIX = 'I am there'
+BUDGET = Ustruct.new(id: 123_456)
+def given
+  { b: 'bonjour le monde' }
+end
 
-  before do
+def absolute
+  { a: 'Hello world' }
+end
 
-    module Presenters
-      module Presenters::Given; end
-      module Presenters::Absolute; end
-      module Presenters::AbsoluteLists; end
-      module Presenters::AbsoluteList; end
-    end
+module Presenters
+  module Given; end
+  module Absolute; end
+  module AbsoluteLists; end
+  module AbsoluteList; end
+end
 
+class TestExposer < DryExposer::Base
+  # include Deps[the_called: 'actions.budgets.read_query']
+  the_called = ->(_id) { BUDGET } # faking call
 
-    class TestExposer < DryExposer::Base
-      #include Deps[ the_called: 'actions.budgets.read_query' ]
-      the_called = -> ( id ) { BUDGET } # faking call
-
-      expose :given, 'fake'
-      expose :absolute, ABSOLUTE, with: :given
-      expose :absolute_callable, -> { ABSOLUTE }
-      expose :absolute_callable_with_arg, -> (id:) { my_method id }
-      expose :relative do |id:|
-        my_method id
-      end
-      expose :relative_callable do |context:|
-        the_called.call( context.id )
-      end
-      expose :other_relative_callable do |id:|
-        the_called.call( id )
-      end
-      expose :absolute_lists, [ ABSOLUTE ]
-      expose :with_absolute_lists, [ ABSOLUTE ], with: [ :given, :absolute ]
-      expose :use_other do |relative:|
-        [ relative, SUFFIX ].join ' '
-      end
-
-      def my_method( id )
-        [ ABSOLUTE, id ].join
-      end
-    end
+  expose :given, 'fake'
+  expose :absolute, absolute, with: :absolute
+  expose :absolute_callable, -> { absolute }
+  expose :absolute_callable_with_arg, ->(id:) { my_method id }
+  expose :relative do |id:|
+    my_method id
+  end
+  expose :relative_callable do |context:|
+    the_called.call(context.id)
+  end
+  expose :other_relative_callable do |id:|
+    the_called.call(id)
+  end
+  expose :absolute_lists, [absolute]
+  expose :with_absolute_lists, [absolute], with: %i[given absolute]
+  expose :use_other do |relative:|
+    [relative, SUFFIX].join ' '
   end
 
-  describe "A unique test" do
+  def my_method(id)
+    [absolute, id].join
+  end
+end
 
+describe DryExposer do
+  describe 'A unique test' do
     it 'works' do
       @budget = BUDGET
       exposer = TestExposer.new
       expected = {
-        given: ABSOLUTE,
-        absolute: ABSOLUTE,
-        absolute_callable: ABSOLUTE,
-        absolute_callable_with_arg: [ABSOLUTE, @budget.id].join,
-        relative: [ABSOLUTE, @budget.id].join,
+        given: given,
+        absolute: absolute,
+        absolute_callable: absolute,
+        absolute_callable_with_arg: [absolute, @budget.id].join,
+        relative: [absolute, @budget.id].join,
         relative_callable: @budget,
         other_relative_callable: @budget,
-        absolute_lists: [ ABSOLUTE ],
-        with_absolute_lists: [ ABSOLUTE ],
-        use_other: [ [ABSOLUTE, @budget.id].join, SUFFIX ].join( ' ' )
+        absolute_lists: [absolute],
+        with_absolute_lists: [absolute],
+        use_other: [[absolute, @budget.id].join, SUFFIX].join(' ')
       }
 
       result = exposer.call(
-        "entity", Ustruct.new( given: ABSOLUTE, id: @budget.id )
+        'entity', Ustruct.new(given: given, id: @budget.id)
       )
 
       # expected results
-      _( result ).must_equal expected
+      _(result).must_equal expected
 
       # expected decoration : result is object
       _(result[:given]).must_have_ancestor Presenters::Given
